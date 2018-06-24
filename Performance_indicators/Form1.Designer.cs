678dd7748a53b3bdfd0d3936b637b0337d066651
alpaca <36502013+Alpaca-Tech@users.noreply.github.com>
2018-06-24 17:15:29 +0800

    Add files via upload

---
+++ Performance_indicators/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace Performance_indicators
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bt_run = new System.Windows.Forms.Button();
+            this.tb_name = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lb_show = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // bt_run
+            // 
+            this.bt_run.Font = new System.Drawing.Font("新細明體", 20F);
+            this.bt_run.Location = new System.Drawing.Point(12, 84);
+            this.bt_run.Name = "bt_run";
+            this.bt_run.Size = new System.Drawing.Size(410, 53);
+            this.bt_run.TabIndex = 0;
+            this.bt_run.Text = "Run";
+            this.bt_run.UseVisualStyleBackColor = true;
+            this.bt_run.Click += new System.EventHandler(this.bt_run_Click);
+            // 
+            // tb_name
+            // 
+            this.tb_name.Font = new System.Drawing.Font("新細明體", 20F);
+            this.tb_name.Location = new System.Drawing.Point(12, 39);
+            this.tb_name.Name = "tb_name";
+            this.tb_name.Size = new System.Drawing.Size(411, 39);
+            this.tb_name.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體", 20F);
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(411, 27);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "輸入班級成績的檔案名稱(input.txt)";
+            // 
+            // lb_show
+            // 
+            this.lb_show.AutoSize = true;
+            this.lb_show.Font = new System.Drawing.Font("新細明體", 20F);
+            this.lb_show.Location = new System.Drawing.Point(12, 158);
+            this.lb_show.Name = "lb_show";
+            this.lb_show.Size = new System.Drawing.Size(66, 27);
+            this.lb_show.TabIndex = 3;
+            this.lb_show.Text = "show";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(434, 450);
+            this.Controls.Add(this.lb_show);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tb_name);
+            this.Controls.Add(this.bt_run);
+            this.Name = "Form1";
+            this.Text = "Performance indicators";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bt_run;
+        private System.Windows.Forms.TextBox tb_name;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lb_show;
+    }
+}
+
---
+++ Performance_indicators/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Performance_indicators
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lb_show.Text = "";
+            tb_name.Text = "input1.txt";
+        }
+
+        private void bt_run_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                FileStream fs_file = new FileStream(tb_name.Text, FileMode.Open, FileAccess.Read);
+                StreamReader fs_reader = new StreamReader(fs_file);
+                string f_read = fs_reader.ReadToEnd();  //read all line
+                string my_read = f_read.Split('\n')[1]; //抓取第二行
+                string[] my_result = my_read.Split(' '); //空格間格 to array
+                my_result = bubble_sort(my_result);       //泡沫排序
+                lb_show.Text = "";
+                
+                foreach (var my_string in my_result)
+                {
+                    lb_show.Text += my_string + ", ";
+                }
+                lb_show.Text = lb_show.Text.Substring(0, lb_show.Text.Length - 2) + "\r\n";
+
+                my_result = my_hl_s(my_result);
+                switch (my_result[2])
+                {
+                    case "0":   //最低及格my_result[0];   最高不及格my_result[1]
+                        lb_show.Text += "最高不及格分數：" + my_result[1] + "\r\n最低及格分數：" + my_result[0];
+                        break;
+
+                    case "-1":  //全部不及格
+                        lb_show.Text += "最高不及格分數：" + my_result[1] + "\r\n" + "最低及格分數：無\r\n笑死，全班不及格";
+                        break;
+
+                    case "-2":  //全部及格
+                        lb_show.Text += "最高不及格分數：無\r\n" + "最低及格分數：" + my_result[0]+"\r\n很棒喔，居然都及格了";
+                        break;
+
+                    default:
+                        break;
+                }
+
+
+
+            }
+            catch { MessageBox.Show("Error!", "Error!"); }
+        }
+
+        string[] bubble_sort(string[] my_result)
+        {
+            int arr_len = my_result.Length;
+            int i, j, temp;
+            int[] my_result_i = Array.ConvertAll<string, Int32>(my_result, value => Int32.Parse(value));    //轉換array字串自int
+            
+            for (i=0; i<(arr_len - 1); i++)
+            {
+                for (j=0; j<(arr_len - 1-i); j++)
+                {
+                    if (my_result_i[j] > my_result_i[j + 1])
+                    {
+                        temp = my_result_i[j]; my_result_i[j] = my_result_i[j + 1]; my_result_i[j + 1] = temp;
+                    }
+                }
+            }
+            my_result = Array.ConvertAll<Int32, string>(my_result_i, value => value.ToString());    //轉換array數字自string
+            return my_result;
+        }/**/
+
+        string[] my_hl_s(string[] my_result)
+        {
+            int arr_len = my_result.Length;
+            int[] my_result_i = Array.ConvertAll<string, Int32>(my_result, value => Int32.Parse(value));    //轉換array字串自int
+            int[] owo = new int[3] { -1, -1, 0}; //一個用於存放return值的廢物容器OuO     owo[0]=最低及格; owo[1]=最高不及格;  owo[2]=例外;
+
+            for (int i=0; i<arr_len; i++)
+            {
+                if(my_result_i[(arr_len-1-i)] >= 60) { owo[0] = my_result_i[(arr_len - 1 - i)]; }   //最低及格
+                if (my_result_i[i] < 60) { owo[1] = my_result_i[i]; }                               //最高不及格
+            }
+            
+
+            if(owo[0] == -1) { owo[2] = -1; /*全部不及格*/}
+            if (owo[1] == -1) { owo[2] = -2; /*全部及格*/}
+            /* owo[2]=0 */
+
+            my_result = Array.ConvertAll<Int32, string>(owo, value => value.ToString());    //轉換array字串自int
+            return my_result;
+        }
+    }
+}
